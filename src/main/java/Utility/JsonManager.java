07c0a816e692bb33ce0504482ef5cd9ba34e8354
Lange99 <christianlangellotti@gmail.com>
2021-06-28 17:17:34 +0200

    showPetriNet e priority rifatto GGWP

--- src/main/java/Utility/JsonManager.java
+++ src/main/java/Utility/JsonManager.java
@@ -175,6 +175,6 @@ public class JsonManager { public static final String INSERT_THE_ID_OF_THE_FILE_
 
     //VERSIONE 5
     public static void loadFileFromAnyPath(String pathSelected) {
-        loa
+
     }
 }
