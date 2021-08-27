10153cc2377e426f0220dee34196b48639456b47
ilNick99 <n.giacomini001@studenti.unibs.it>
2021-08-27 15:23:03 +0200

    Aggiunta l'interfaccia StrategyNetManager che permette di applicare il pattern Strategy

---
+++ src/main/java/Project_v5/StrategyNetManager.java
@@ -0,0 +1,10 @@
+package main.java.Project_v5;
+
+import java.io.FileNotFoundException;
+
+public interface StrategyNetManager {
+    boolean checkLoadGenericNet(PetriNet petriNet) throws FileNotFoundException;
+    boolean checkLoadGenericNet(PriorityPetriNet priorityPetriNet) throws FileNotFoundException;
+    boolean checkNetName(Net net);
+    boolean checkNetName(PetriNet net);
+}
