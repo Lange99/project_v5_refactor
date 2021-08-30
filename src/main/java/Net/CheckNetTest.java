8e116e38de1349cc06e403b1a604fd966bb89f54
IlariaPasini <i.pasini001@studenti.unibs.it>
2021-08-30 15:08:02 +0200

    finito testing

--- src/main/java/Net/CheckNetTest.java
+++ src/main/java/Net/CheckNetTest.java
@@ -1,21 +1,36 @@
 package main.java.Net;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
+import static java.util.function.Predicate.isEqual;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import java.lang.*;
 class CheckNetTest {
 
+
+    private Object AssertionError;
+
     @org.junit.jupiter.api.Test
-    void checkTransIncorrect() {
-        Net n=new main.java.Net.Net("ReteProva");
+    void checkTransNoPre() {
+        Net n=new Net("ReteProva");
         Transition t=new Transition("T1");
         Place p= new Place("BRESCIA");
         n.addPair(t,p,1);
         assertFalse(CheckNet.checkTrans(n));
     }
+    @org.junit.jupiter.api.Test
+    void checkTransNoPost() {
+        Net n=new Net("ReteProva");
+        Transition t=new Transition("T1");
+        Place p= new Place("BRESCIA");
+        n.addPair(t,p,0);
+        assertFalse(CheckNet.checkTrans(n));
+    }
 
     @org.junit.jupiter.api.Test
-    void checkTransCorrectTrans() {
-        Net n=new main.java.Net.Net("ReteProva1");
+    void checkTransCorrectTransOneTrans() {
+        Net n=new Net("ReteProva1");
         Transition t=new Transition("T1");
         Place p= new Place("BRESCIA");
         Place p1= new Place("BERGAMO");
@@ -23,10 +38,30 @@ class CheckNetTest {
         n.addPair(t,p,1);
         assertTrue(CheckNet.checkTrans(n));
     }
-
     @org.junit.jupiter.api.Test
-    void checkTransInorrectTransMore() {
-        Net n=new main.java.Net.Net("ReteProva2");
+    void checkTransCorrectTransMoreTrans() {
+        Net n=new Net("ReteProva2");
+        Transition t=new Transition("T0");
+        Transition t1=new Transition("T1");
+        Transition t2=new Transition("T2");
+        Transition t3=new Transition("T3");
+        Place BS= new Place("BRESCIA");
+        Place BG= new Place("BERGAMO");
+        Place MI= new Place("MILANO");
+        Place MA= new Place("MANTOVA");
+        n.addPair(t,BG,0);
+        n.addPair(t,BS,1);
+        n.addPair(t1,BS,0);
+        n.addPair(t1,MI,1);
+        n.addPair(t2,MA,0);
+        n.addPair(t2,BS,1);
+        n.addPair(t3,MI,0);
+        n.addPair(t3,BG,1);
+        assertTrue(CheckNet.checkTrans(n));
+    }
+    @org.junit.jupiter.api.Test
+    void checkTransNoPostWithMoreThanOneElement() {
+        Net n=new Net("ReteProva3");
         Transition t=new Transition("T1");
         Transition t1=new Transition("T2");
         Place p= new Place("BRESCIA");
@@ -36,4 +71,31 @@ class CheckNetTest {
         n.addPair(t1,p1,0);
         assertFalse(CheckNet.checkTrans(n));
     }
+    @org.junit.jupiter.api.Test
+    void checkTransNoPreWithMoreThanOneElement() {
+        Net n=new Net("ReteProva4");
+        Transition t=new Transition("T1");
+        Transition t1=new Transition("T2");
+        Place p= new Place("BRESCIA");
+        Place p1= new Place("BERGAMO");
+        n.addPair(t,p1,0);
+        n.addPair(t,p,1);
+        n.addPair(t1,p1,1);
+        assertFalse(CheckNet.checkTrans(n));
+    }
+    @Test
+    void checkTransNoElement() {
+        Net n=new Net("ReteProva5");
+      //  assertThat(, (AssertionError));
+       try {
+           CheckNet.checkTrans(n);
+       }catch (AssertionError e){
+           assertTrue(true);
+           return;
+       }
+        assertTrue(false);
+    }
+
+
+
 }
